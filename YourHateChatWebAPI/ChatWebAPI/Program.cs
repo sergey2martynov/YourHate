adec838dadf7d5746978999e830a8cdae0e2a162
sergey2martynov <94121806+sergey2martynov@users.noreply.github.com>
2023-03-02 12:05:04 +0300

    Created chat web api

--- YourHateChatWebAPI/ChatWebAPI/Program.cs
+++ YourHateChatWebAPI/ChatWebAPI/Program.cs
@@ -1,9 +1,11 @@
+using ChatData;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddPersistance(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
