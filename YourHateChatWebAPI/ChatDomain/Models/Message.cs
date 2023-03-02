4be0ef368a886d3c19d577b2c445783d30d9f513
sergey2martynov <94121806+sergey2martynov@users.noreply.github.com>
2023-03-02 12:04:46 +0300

    Created message model

---
+++ YourHateChatWebAPI/ChatDomain/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatData.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime Created { get; set; }
+        public int LikeCount { get; set; }
+        public string Content { get; set; }
+    }
+}
